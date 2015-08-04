65516e366f6a68c6eaf0fe1df2e19bcb98a54342
Eli <efwb001@gmail.com>
2015-08-04 12:36:22 -0400

    Fix for StdErr when it receives a 1D vector and it's transposed of what's expected

--- StdErr.m
+++ StdErr.m
@@ -1,3 +1,7 @@
 function [stdErr] = StdErr (data)
-    stdErr = std(data, 0, 1) / sqrt(length(data));
+    if sum(size(data)>1) > 1 %just to be clear
+        stdErr = std(data, 0, 1) / sqrt(length(data));
+    else
+        stdErr = std(data, 0) / sqrt(length(data));
+    end
 end
