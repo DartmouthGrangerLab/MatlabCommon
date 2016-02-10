82a0f4ff21f8c78aa3cc8324c41581b1c0bb0f9a
Eli <efwb001@gmail.com>
2016-02-10 12:28:26 -0500

    Small enhancements. A new function ClusterOptimalK for traditional cluster analysis.

--- figurecode/PlotDSM.m
+++ figurecode/PlotDSM.m
@@ -18,14 +18,16 @@ function [h] = PlotDSM (h, mat, labels, precision)
     imagesc(mat); %create a colored plot of the matrix values
     colormap(flipud(gray)); %change the colormap to gray (so higher values are black and lower values are white)
 
-    textStrings = num2str(mat(:), ['%0.',num2str(precision),'f']); %create strings from the matrix values
-    textStrings = strtrim(cellstr(textStrings)); %remove any space padding
-    [x,y] = meshgrid(1:size(mat, 1)); %create x and y coordinates for the strings
-    hStrings = text(x(:), y(:), textStrings(:), 'HorizontalAlignment', 'center'); %plot the strings
-    midValue = mean(get(gca,'CLim')); %get the middle value of the color range
-    textColors = repmat(mat(:) > midValue, 1, 3); %choose white or black for the text color of the strings so
-                                                  %they can be easily seen over the background color
-    set(hStrings, {'Color'}, num2cell(textColors, 2)); %change the text colors
+    if numel(labels) < 20
+        textStrings = num2str(mat(:), ['%0.',num2str(precision),'f']); %create strings from the matrix values
+        textStrings = strtrim(cellstr(textStrings)); %remove any space padding
+        [x,y] = meshgrid(1:size(mat, 1)); %create x and y coordinates for the strings
+        hStrings = text(x(:), y(:), textStrings(:), 'HorizontalAlignment', 'center'); %plot the strings
+        midValue = mean(get(gca,'CLim')); %get the middle value of the color range
+        textColors = repmat(mat(:) > midValue, 1, 3); %choose white or black for the text color of the strings so
+                                                      %they can be easily seen over the background color
+        set(hStrings, {'Color'}, num2cell(textColors, 2)); %change the text colors
+    end
 
     set(gca, 'XTick', 1:size(mat, 1), ...  %change the axes tick marks
             'XTickLabel', labels, ...      %and tick labels
--- statistics/StdErr.m
+++ statistics/StdErr.m
@@ -2,7 +2,7 @@
 %   data - vector of data points to test
 function [stdErr] = StdErr (data)
     if sum(size(data)>1) > 1 %just to be clear
-        stdErr = std(data, 0, 1) / sqrt(length(data));
+        stdErr = std(data, 0, 1) / sqrt(size(data, 1));
     else
         stdErr = std(data, 0) / sqrt(length(data));
     end
