634d53b5b21e3f3331ad76ac7785639c6a9146a3
Eli <efwb001@gmail.com>
2017-10-30 12:40:38 -0400

    Improved speed and matlabiness (clarity) of BinaryPearson and PointBiserial

--- statistics/BinaryPearson.m
+++ statistics/BinaryPearson.m
@@ -4,17 +4,21 @@
 %INPUTS:
 %   d - N-dimensional vector (binary)
 %   x - N x numMIs (binary)
-function [retVal] = BinaryPearson (d, x)
+%   stdOfX - OPTIONAL - precomputed value of exactly std(x, 1) - only useful for speeding up multiple calls with the same X
+function [retVal] = BinaryPearson (d, x, stdOfX)
     assert(islogical(d), 'd must be binary (boolean)');
     assert(islogical(x), 'x must be binary (boolean)');
 %     numMIs = size(x, 2);
     N = size(d, 1); %Length
     
-    stdOfX = std(x, 1); %TODO: should be numMIs x 1 - population standard devation for each of D columns in x
+    if ~exist('stdOfX', 'var') || isempty(stdOfX)
+        stdOfX = std(x, 1); %TODO: should be numMIs x 1 - population standard devation for each of D columns in x
+    end
+    
     countD = sum(d);
     
     countX = sum(x, 1);
-    countDX = sum(x(d==1,:), 1);
+    countDX = sum(x(d,:), 1);
     
     stdVal = std(d); %calculate stddev for d (a scalar)
     cov = (N .* countDX - countD.*countX) ./ (N*N);
--- statistics/PointBiserial.m
+++ statistics/PointBiserial.m
@@ -4,25 +4,27 @@
 %INPUTS:
 %   d - N-dimensional vector (binary)
 %   x - N x numMIs (real valued)
-function [retVal] = PointBiserial (d, x)
+%   stdOfX - OPTIONAL - precomputed value of exactly std(x, 1) - only useful for speeding up multiple calls with the same X
+function [retVal] = PointBiserial (d, x, stdOfX)
     assert(islogical(d), 'd must be binary (boolean)');
-%     numMIs = size(x, 2);
     N = size(d, 1); %Length
-%     xMeans = zeros(2, numMIs); %Mean of groups 0 and 1
     
-    stdOfX = std(x, 1); %numMIs x 1 - population standard devation for each of D columns in x
+    if ~exist('stdOfX', 'var') || isempty(stdOfX)
+        stdOfX = std(x, 1); %numMIs x 1 - population standard devation for each of D columns in x
+    end
 
-    n0 = sum(d==0);
-    n1 = sum(d==1);
+    n0 = sum(~d);
+    n1 = sum(d);
     assert(n0~=0 && n1~=0, 'There is only one group!');
+%     xMeans = zeros(2, numMIs); %Mean of groups 0 and 1
 %     for i = 1:N
 %         xMeans(d(i)+1,:) = xMeans(d(i)+1,:) + x(i,:);
 %     end
 %     xMeans(1,:) = xMeans(1,:) ./ n0;
 %     xMeans(2,:) = xMeans(2,:) ./ n1;
-    xMeans(1,:) = mean(x(d==0,:), 1);
-    xMeans(2,:) = mean(x(d==1,:), 1);
+    xMeans0 = mean(x(~d,:), 1);
+    xMeans1 = mean(x(d,:), 1);
     
     c = sqrt(n0*n1/(N*N)); %scalar
-    retVal = (xMeans(2,:)-xMeans(1,:)) ./ stdOfX * c; %Correlation coefficient
+    retVal = (xMeans1-xMeans0) ./ stdOfX * c; %Correlation coefficient
 end
