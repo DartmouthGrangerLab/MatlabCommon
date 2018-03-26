e95e9f78287bb4f1a1d89db564807b7268ba3a4a
Eli <efwb001@gmail.com>
2018-03-26 14:50:14 -0400

    Improving readibility of NMI2.m and improving reliability of CalinskiHarabasz statistic

--- statistics/ClusterEvalCalinskiHarabasz.m
+++ statistics/ClusterEvalCalinskiHarabasz.m
@@ -1,14 +1,21 @@
 %Eli Bowen (copied directly from matlab's clustering.evaluation.CalinskiHarabaszEvaluation code to make a standalone function)
 %12/17/16
 %CalinskiHarabasz cluster criterion / variance ratio criterion
+%this measure uses Euclidean distance, and is best used when clusters are relatively spherical (if you care)
+%higher is better, range is (0,Inf)
+%This is roughly identical to the anova F-test (https://en.wikipedia.org/wiki/F-test)
+%When Rick says do a "within vs between cluster" test, this is it. Silhouette coefficient will also suffice, and is more flexible.
 %INPUTS:
 %   X - NxD position of each data point
 %   clustAssignments - 1D vector (N elements) cluster ID assigned to each datapoint (e.g. a value of 2 indicates a point was assigned to the centroid with mean at the second row of 'centroids')
 function [ch] = ClusterEvalCalinskiHarabasz (X, clustAssignments)
-    assert(~any(any(isnan(X))), 'X contains NaN values. TODO: code a way to handle this');
-    assert(numel(unique(clustAssignments)) > 1, 'Only one cluster has assignments!');
+    assert(~any(isnan(X(:))), 'X contains NaN values. TODO: code a way to handle this');
+    if numel(unique(clustAssignments)) == 1
+        ch = NaN; %only one cluster has assignments!
+        return;
+    end
 	
-    clusts = unique(clustAssignments);
+    [clusts,~,clustIdx] = unique(clustAssignments);
     K = numel(clusts);
     [N,D] = size(X);
     globalMean = mean(X, 1);
@@ -17,17 +24,23 @@ function [ch] = ClusterEvalCalinskiHarabasz (X, clustAssignments)
     Ni = zeros(K, 1); %number of points in each cluster
     sumD = zeros(K, 1); %sum of Squared Euclidean
     for i = 1:K
-        members = (clustAssignments == i);
+        members = (clustIdx == i);
         centroids(i,:) = mean(X(members,:), 1);
         Ni(i) = sum(members);
         sumD(i) = sum(pdist2(X(members,:), centroids(i,:)) .^ 2);
     end
     
+    if sum(Ni>1) < 2
+        ch = NaN; %only one cluster has >1 assignment
+        return;
+    end
+    
     %% Within cluster variance
-	SSW = sum(sumD, 1);
+	SSW = sum(sumD(Ni>1)); %Eli: removing 1-point clusters since their within-cluster variance is undefined
     
     %% Between cluster variance
 	SSB = pdist2(centroids, globalMean) .^ 2;
-	SSB = sum(Ni .* SSB);
-	ch =(SSB/(K-1)) / (SSW/(N-K));
+	SSB = sum(Ni(Ni>1) .* SSB(Ni>1)); %Eli: removing 1-point clusters since their within-cluster variance is undefined
+    K = K-sum(Ni==1); %Eli: removing 1-point clusters since their within-cluster variance is undefined
+	ch = (SSB/(K-1)) / (SSW/(N-K));
 end
--- statistics/NMI2.m
+++ statistics/NMI2.m
@@ -36,12 +36,12 @@
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 % POSSIBILITY OF SUCH DAMAGE.
 %Modified by Eli Bowen from AMI function to only calculate NMI, return 
-function [nmi,MI] = NMI2 (true_mem, mem)
+function [NMI,MI] = NMI2 (true_mem, mem)
     if nargin == 1
         T = true_mem; %contingency table pre-supplied
     elseif nargin == 2
         %added by Eli Bowen 4/8/2017 for safety
-        assert(sum(size(true_mem)>1)==1 && sum(size(mem)>1)==1 && numel(true_mem)==numel(mem), 'With 2 arguments, both arguments must be 1D and of equal length');
+        assert(isvector(true_mem) && isvector(mem) && numel(true_mem)==numel(mem), 'With 2 arguments, both arguments must be 1D and of equal length');
         assert(min(true_mem) > 0 && min(mem) > 0, 'Category IDs must be > 0');
         
         %build the contingency table from membership arrays
@@ -55,45 +55,52 @@ function [nmi,MI] = NMI2 (true_mem, mem)
         T = T(list_t, list_m);
     end
 
-    n = sum(sum(T));
+    N = sum(sum(T));
     
     %update the true dimensions
     [R,C] = size(T);
-    if C>1 a=sum(T');else a=T';end;
-    if R>1 b=sum(T);else b=T;end;
+    if C > 1
+        a = sum(T');
+    else
+        a = T';
+    end
+    if R > 1
+        b = sum(T);
+    else
+        b = T;
+    end
 
     %calculating the Entropies
-    Ha = -(a/n)*log(a/n)'; 
-    Hb = -(b/n)*log(b/n)';
+    Ha = -(a/N) * log(a/N)'; 
+    Hb = -(b/N) * log(b/N)';
 
     %calculate the MI (unadjusted)
     MI = 0;
     for i = 1:R
         for j = 1:C
             if T(i,j) > 0
-                MI = MI + T(i,j)*log(T(i,j)*n/(a(i)*b(j)));
+                MI = MI + T(i,j) * log(T(i,j)*N/(a(i)*b(j)));
             end
         end
     end
-    MI = MI / n;
-    
+    MI = MI / N;
     if MI == 0
-        nmi = 0;
+        NMI = 0;
     else
-        nmi = MI / sqrt(Ha*Hb);
+        NMI = MI / sqrt(Ha*Hb);
     end
 end
 
 %---------------------auxiliary functions---------------------
 function [Cont] = Contingency (Mem1, Mem2)
     if nargin < 2 || min(size(Mem1)) > 1 || min(size(Mem2)) > 1
-       error('Contingency: Requires two vector arguments');
+        error('Contingency: Requires two vector arguments');
     end
 
     Cont = zeros(max(Mem1), max(Mem2));
 
     for i = 1:length(Mem1);
-       Cont(Mem1(i),Mem2(i)) = Cont(Mem1(i),Mem2(i)) + 1;
+        Cont(Mem1(i),Mem2(i)) = Cont(Mem1(i),Mem2(i)) + 1;
     end
 end
 
