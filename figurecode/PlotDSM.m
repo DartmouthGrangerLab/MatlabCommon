a74365b7a3c66695251cf8e089b00308d912f0f4
Eli <efwb001@gmail.com>
2018-05-10 14:09:38 -0400

    Adding a 3D heatmap (for 4D data) and updating PlotDSM to work with non-square matrices

--- figurecode/PlotDSM.m
+++ figurecode/PlotDSM.m
@@ -2,13 +2,20 @@
 %modified by Eli Bowen 1/2016
 %INPUTS:
 %   h - figure handle. If empty, will create one.
-%   mat - 
-%   labels - text to display for each category (cell array of strings)
+%   mat - YxX matrix
+%   xLabels - (OPTIONAL) text to display for each category along the X axis (cell array of strings)
+%   yLabels - (OPTIONAL) text to display for each category along the Y axis (cell array of strings)
 %   precision - (OPTIONAL) number of decimal points to render. default = 4.
 %   fontSize - (OPTIONAL) font size for the text. Can leave blank for matlab defaults of 10.
-function [h] = PlotDSM (h, mat, labels, precision, fontSize)
-    assert(size(mat, 1) == size(mat, 2), 'mat must be a square matrix!');
-    if exist('precision', 'var') && isempty(precision)
+%EFWB 4/26/2018: changing so doesn't need to be a square matrix. big change is instead of one param "labels" we now have 2: "xLabels" and "yLabels"
+function [h] = PlotDSM (h, mat, xLabels, yLabels, precision, fontSize)
+    if exist('xLabels', 'var') && ~isempty(xLabels)
+        assert(numel(xLabels) == size(mat, 2));
+    end
+    if exist('yLabels', 'var') && ~isempty(yLabels)
+        assert(numel(yLabels) == size(mat, 1));
+    end
+    if ~exist('precision', 'var') || isempty(precision)
         precision = 4;
     end
     if ~exist('fontSize', 'var') || isempty(fontSize)
@@ -19,23 +26,37 @@ function [h] = PlotDSM (h, mat, labels, precision, fontSize)
         h = figure();
     end
     set(h, 'Renderer', 'painters');
+    
+    %% print matrix
     imagesc(mat); %create a colored plot of the matrix values
     colormap(flipud(gray)); %change the colormap to gray (so higher values are black and lower values are white)
-
-    if numel(labels) < 20
+    
+    %% print numbers inside cells
+    if size(mat, 1) < 20 && size(mat, 2) < 20
         textStrings = num2str(mat(:), ['%0.',num2str(precision),'f']); %create strings from the matrix values
         textStrings = strtrim(cellstr(textStrings)); %remove any space padding
-        [x,y] = meshgrid(1:size(mat, 1)); %create x and y coordinates for the strings
+        [x,y] = meshgrid(1:size(mat, 2), 1:size(mat, 1)); %create x and y coordinates for the strings (yes the dimensions seem reversed)
         hStrings = text(x(:), y(:), textStrings(:), 'HorizontalAlignment', 'center', 'FontSize', fontSize); %plot the strings (font size 10 = default)
         midValue = mean(get(gca,'CLim')); %get the middle value of the color range
         textColors = repmat(mat(:) > midValue, 1, 3); %choose white or black for the text color of the strings so
                                                       %they can be easily seen over the background color
         set(hStrings, {'Color'}, num2cell(textColors, 2)); %change the text colors
     end
-
-    set(gca, 'XTick', 1:size(mat, 1), ...  %change the axes tick marks
-            'XTickLabel', labels, ...      %and tick labels
-            'YTick', 1:size(mat, 1), ...
-            'YTickLabel', labels, ...
-            'TickLength', [0 0]);
+    
+    %% print text labels
+%     set(gca, 'XTick', 1:size(mat, 2), ...  %change the axes tick marks
+%             'XTickLabel', xLabels, ...      %and tick labels
+%             'YTick', 1:size(mat, 1), ...
+%             'YTickLabel', yLabels, ...
+%             'TickLength', [0 0]);
+    ax = gca();
+    ax.XTick = 1:size(mat, 2); %change the axes tick marks
+    if exist('xLabels', 'var') && ~isempty(xLabels)
+        ax.XTickLabel = xLabels;
+    end
+    ax.YTick = 1:size(mat, 1);
+    if exist('yLabels', 'var') && ~isempty(yLabels)
+        ax.YTickLabel = yLabels;
+    end
+    ax.TickLength = [0,0];
 end
