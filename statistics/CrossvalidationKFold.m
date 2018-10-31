1c76a4eeaef54bb4945398ddbd916667334edb65
Eli <efwb001@gmail.com>
2018-10-31 12:57:51 -0400

    Crossvalidation splits of the data now must be specified as random or not - clears up prior confusion

--- statistics/CrossvalidationKFold.m
+++ statistics/CrossvalidationKFold.m
@@ -1,14 +1,14 @@
 %implements k-fold cross-validation
 %INPUTS:
 %   labels - 1D numeric array of label IDs for each datapoint
-%   numFolds - (OPTIONAL) number of folds (defualt = 10)
+%   numFolds - number of folds (e.g. 10)
+%   beRandom - OPTIONAL - iff true, datapoints will be randomly shuffled before selection (used to always be false)
 %RETURNS:
 %   trainIndices - indices into labels array (aka positions of datapoints) for training points, one cell per fold
 %   testIndices - indices into labels array (aka positions of datapoints) for testing points, one cell per fold
-function [trainIndices, testIndices] = CrossvalidationKFold (labels, numFolds)
-    if ~exist('numFolds','var') || isempty(numFolds)
-        numFolds = 10;
-    end
+function [trainIndices, testIndices] = CrossvalidationKFold (labels, numFolds, beRandom)
+    assert(isnumeric(labels) && isvector(labels));
+    assert(isnumeric(numFolds) && isscalar(numFolds));
 
     uniqueLabels = unique(labels);
     trainIndices = cell(numFolds, 1);
@@ -20,6 +20,9 @@ function [trainIndices, testIndices] = CrossvalidationKFold (labels, numFolds)
     for k = 1:numel(uniqueLabels)
         catIndices = find(labels == uniqueLabels(k));
         N = numel(catIndices);
+        if beRandom
+            catIndices = catIndices(randperm(numel(catIndices)));
+        end
         for fold = 1:numFolds
             trainIndices{fold} = [trainIndices{fold};catIndices(1:ceil((fold-1)*N/numFolds))];
             trainIndices{fold} = [trainIndices{fold};catIndices(ceil(fold*N/numFolds)+1:N)];
--- statistics/CrossvalidationLeaveKOut.m
+++ statistics/CrossvalidationLeaveKOut.m
@@ -2,11 +2,14 @@
 %INPUTS:
 %   labels - 1D numeric array of label IDs for each datapoint
 %   numValidations - number of bootstrapped cross-validations to perform
-%   fractionTest - (DEFAULT = 0.5) number between 0 and 1, describing what fraction of the data for each class should be used for testing
+%   fractionTest - OPTIONAL - number between 0 and 1, describing what fraction of the data for each class should be used for testing (DEFAULT = 0.5)
+%   beRandom - iff true, datapoints will be randomly shuffled before selection (used to always be true)
 %RETURNS:
 %   trainIndices - indices into labels array (aka positions of datapoints) for training points, one cell per validation trial
 %   testIndices - indices into labels array (aka positions of datapoints) for testing points, one cell per validation trial
-function [trainIndices, testIndices] = CrossvalidationLeaveKOut (labels, numValidations, fractionTest)
+function [trainIndices, testIndices] = CrossvalidationLeaveKOut (labels, numValidations, fractionTest, beRandom)
+    assert(isnumeric(labels) && isvector(labels));
+    assert(isnumeric(numValidations) && isscalar(numValidations));
     if ~exist('fractionTest','var') || isempty(fractionTest)
         fractionTest = 0.5;
     end
@@ -23,8 +26,9 @@ function [trainIndices, testIndices] = CrossvalidationLeaveKOut (labels, numVali
             catIndices = find(labels == uniqueLabels(k));
             nTest = ceil(numel(catIndices) * fractionTest); %num test
             nTrain = numel(catIndices) - nTest; %num train
-            [randIndices] = randperm(numel(catIndices));
-            catIndices = catIndices(randIndices);
+            if beRandom
+                catIndices = catIndices(randperm(numel(catIndices)));
+            end
 
             trainIndices{trial} = vertcat(trainIndices{trial}, catIndices(1:nTrain));
 
