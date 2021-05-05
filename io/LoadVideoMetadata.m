bda651d6f7831189ff5745478a2819020a82d6e5
Eli <efwb001@gmail.com>
2021-05-05 11:31:05 -0400

    Adding a LoadVideoMetadata function that is much faster than loading a video

--- io/LoadVideoMetadata.m
+++ io/LoadVideoMetadata.m
@@ -1,19 +1,16 @@
 % Eli Bowen
-% 8/3/2020
-% simple function for loading a video all at once in bulk
-% supports:
+% 4/29/2021
+% simple function for loading a video's metadata
+% much faster than loading the video itself
+%supports:
 %   any file type supported by matlab's VideoReader (hopefully mjpeg in an avi or motion jpeg 2000 in an mj2, or VideoReader compatability will be iffy)
 %   .mat files (must contain a variable called video (uint8 nRows x nCols x nChannels x nFrames) and one called frameRate (scalar numeric)
-% INPUTS:
+%INPUTS:
 %   path
 %   fileName
-%   sz - OPTIONAL - [nRows,nCols] aka [height,width] - size of video; if provided, and video is a different size, we'll resize it automagically
-function [video,frameRate] = LoadVideo (path, fileName, sz)
+function [vidSize,frameRate] = LoadVideoMetadata (path, fileName)
     validateattributes(path, {'char'}, {'nonempty','vector'});
     validateattributes(fileName, {'char'}, {'nonempty','vector'});
-    if exist('sz', 'var') && ~isempty(sz)
-        validateattributes(sz, {'numeric'}, {'nonempty','vector','positive','integer'});
-    end
     
     if endsWith(fileName, '.mat')
         load(fullfile(path, fileName), 'video', 'frameRate');
@@ -30,22 +27,7 @@ function [video,frameRate] = LoadVideo (path, fileName, sz)
         nChan = 3;
     end
     
-    if ~exist('sz', 'var') || isempty(sz)
-        sz = [vidH.Height,vidH.Width];
-    end
-    resize = (sz(1) ~= vidH.Height || sz(2) ~= vidH.Width);
-    
     nFrames = vidH.Duration * frameRate;
 %     nFrames = vidH.NumFrames; % "not always available"
-    video = zeros(sz(1), sz(2), nChan, nFrames, 'uint8');
-    count = 1;
-    while hasFrame(vidH) && count <= nFrames
-        if resize
-            video(:,:,:,count) = imresize(vidH.readFrame(), sz);
-        else
-            video(:,:,:,count) = vidH.readFrame();
-        end
-        count = count + 1;
-    end
-%     video(:,:,:,count:end) = []; % should maybe do this, maybe not
+    vidSize = [vidH.Height,vidH.Width,nChan,nFrames];
 end
--- io/WriteVideo.m
+++ io/WriteVideo.m
@@ -1,12 +1,14 @@
-%Eli Bowen
-%1/7/2021
+% Eli Bowen
+% 1/7/2021
 % writes a video to file (just a wrapper around matlab code to save us time)
-%INPUTS:
+% INPUTS:
 %   filePath - full path to file (including file name) (extension will be corrected if wrong)
 %   video - nCols x nRows x nChannels x nFrames uint8 (range 0-->255) or double (range 0-->1)
 %   frameRate
 %   isLossless - scalar logical (should video be lossless or lossy)
-function [] = WriteVideo (filePath, video, frameRate, isLossless)
+% RETURNS:
+%   filePath - true written file path - extension may have been changed from input filePath
+function [filePath] = WriteVideo (filePath, video, frameRate, isLossless)
     validateattributes(filePath, {'char'}, {'nonempty','vector'});
     validateattributes(video, {'uint8','double'}, {'nonempty','ndims',4});
     validateattributes(frameRate, {'numeric'}, {'nonempty','scalar','positive'});
@@ -15,16 +17,64 @@ function [] = WriteVideo (filePath, video, frameRate, isLossless)
         assert(max(video(:)) <= 1 && min(video(:)) >= 0); % range of double inputs must be 0-->1 (a matlab image convention)
         video = uint8(video .* 255);
     end
-
-    [path,fileName,~] = fileparts(filePath); % strip extension
+    
+    % pad for video player compatability
+    aspectRatio = size(video, 2) / size(video, 1);
+    if ~isLossless
+        rows2Add = 0;
+        cols2Add = 0;
+        if aspectRatio > 16 / 9
+            aspectRatio = 16 / 9; % pad vertically (rows) to reach 16:9
+            rows2Add = round(size(video, 2) / aspectRatio); % yes divide
+        else
+            if aspectRatio < 4 / 3
+                aspectRatio = 4 / 3; % pad horizontally (cols) to reach 4:3
+                cols2Add = round(size(video, 1) * aspectRatio);
+            elseif aspectRatio > 4 / 3 && aspectRatio < 16 / 10 % between 4:3 and 16:10
+                aspectRatio = 16 / 10; % pad horizontally (cols) to reach 16:10
+                cols2Add = round(size(video, 1) * aspectRatio);
+            elseif aspectRatio > 16 / 10 && aspectRatio < 16 / 9 % between 16:10 and 16:9
+                aspectRatio = 16 / 9; % pad horizontally (cols) to reach 16:9
+                cols2Add = round(size(video, 1) * aspectRatio);
+            end
+        end
+        if rows2Add > 0
+            padding1 = zeros(floor(rows2Add/2), size(video, 2), size(video, 3), size(video, 4), 'like', video);
+            padding2 = zeros(ceil(rows2Add/2),  size(video, 2), size(video, 3), size(video, 4), 'like', video);
+            video = [padding1;video;padding2];
+        end
+        if cols2Add > 0
+            padding1 = zeros(size(video, 1), floor(cols2Add/2), size(video, 3), size(video, 4), 'like', video);
+            padding2 = zeros(size(video, 1), ceil(cols2Add/2),  size(video, 3), size(video, 4), 'like', video);
+            video = [padding1,video,padding2];
+        end
+    end
+    
+    % strip extension
+    [path,fileName,requestedExt] = fileparts(filePath);
+    filePath = fullfile(path, fileName);
+    
+    isUseFFMPEG = false;
     if isLossless
-        % using below (lossless mjpeg2000) because video files are tiny and artifacts pop out in retina code
-        v = VideoWriter(fullfile(path, [fileName,'.mj2']), 'Archival');
+        filePath = [filePath,'.mj2'];
+        % using below (lossless mjpeg2000) because video files are tiny and lossy artifacts pop out in retina code
+        v = VideoWriter(filePath, 'Archival');
+        v.MJ2BitDepth = 8;
         % we no longer save lossless as mat files, because turns out matlab loads .mj2 files 2x-10x faster than compressed mat files (and they're the same size at high quality mj2s)
+    elseif strcmpi(requestedExt, 'mp4') && (ispc() || ismac())
+        filePath = [filePath,'.mp4'];
+        v = VideoWriter(filePath, 'MPEG-4');
+        v.Quality = 75; % range [0,100], 100 is best
     else
-        % below, mjpeg200, has smaller files for similar quality vs mjpeg avi (and takes less time to write due to it supporting grayscale)
-        v = VideoWriter(fullfile(path, [fileName,'.mj2']), 'Motion JPEG 2000');
+        filePath = [filePath,'.mj2'];
+        % mjpeg2000 has smaller files for similar quality vs mjpeg avi (and takes less time to write due to it supporting grayscale)
+        v = VideoWriter([filePath,'.mj2'], 'Motion JPEG 2000');
         v.CompressionRatio = 5; % only for motion jpeg 2000, default = 10
+        v.MJ2BitDepth = 8;
+        
+        if strcmpi(requestedExt, 'mp4') && isFFMPEG
+            isUseFFMPEG = true;
+        end
     end
 
     v.FrameRate = frameRate; % fps
@@ -34,4 +84,25 @@ function [] = WriteVideo (filePath, video, frameRate, isLossless)
         writeVideo(v, video(:,:,:,frame));
     end
     close(v);
+    
+    if isUseFFMPEG
+        [~,cmdout] = system('command -v ffmpeg'); % unlikely to succeed on windows
+        if contains(cmdout, 'ffmpeg', 'IgnoreCase', true)
+            if aspectRatio == 4 / 3
+                aspectStr = '-aspect 4:3';
+            elseif aspectRatio == 16 / 10
+                aspectStr = '-aspect 16:10';
+            elseif aspectRatio == 16 / 9
+                aspectStr = '-aspect 16:9';
+            else
+                aspectStr = '';
+            end
+            status = system(['ffmpeg -i "',filePath,'.mj2" -c:v libx265 ',aspectStr,' -crf 5 "',filePath,'.mp4"']);
+            if status == 0
+                system(['rm ',filePath,'.mj2']);
+            else
+                system(['rm ',filePath,'.mp4']); % something went wrong, remove duplicate if present
+            end
+        end
+    end
 end
