421c9ea8be675d14076556b0dc4e5efc6321bf27
Eli <mail@elibowen.net>
2021-11-15 11:35:26 -0500

    Adding a few stats functions

---
+++ statistics/ClassifyHold1Out.m
@@ -0,0 +1,65 @@
+% Eli Bowen
+% 10/1/2021
+% INPUTS:
+%   data - n_pts x n_dims (numeric or logical)
+%   label - 1 x n_pts (int-valued numeric or cell array of chars)
+%   classifierType - 'lda', 'svm', 'svmjava', 'svmliblinear', 'logreg', 'logregliblinear', 'knn'
+%   classifierParams - OPTIONAL struct
+%       .cost - misclassification cost, a KxK matrix where first dim is true label, second dim is predicted label (default: ones(K) - eye(K))
+%       .k - for KNN
+%       .distance - for KNN. e.g. 'euclidean', 'correlation', 'cosine', 'hamming', ...
+%   verbose - OPTIONAL scalar (logical) - should we print text? (default=false)
+% RETURNS:
+%   acc - scalar (double ranged 0 --> 1) - accuracy (mean across folds)
+%   predLabel
+function [acc,predLabel] = ClassifyHold1Out (data, label, classifierType, classifierParams, verbose)
+    validateattributes(data, {'numeric','logical'}, {'nonempty','2d','nonnan','nrows',numel(label)});
+    validateattributes(label, {'numeric'}, {'nonempty','vector'});
+    validateattributes(classifierType, {'char'}, {'nonempty'});
+    if ~exist('verbose', 'var') || isempty(verbose)
+        verbose = false;
+    end
+    
+    %% default classifier params
+    if ~exist('classifierParams', 'var') || isempty(classifierParams)
+        classifierParams = struct();
+        classifierParams.regularization_lvl = 'optimize'; % 1 (default) = much faster, default regularization, may be less successful
+        classifierParams.k = 1;
+        classifierParams.distance = 'euclidean';
+    end
+
+    %% clean up, standardize provided labels
+    % some algs require labels to be the integers 1:numel(uniqueLabels)
+    [uniqueLabel,~,labelNum] = unique(label); % convert labels into index into uniqueLabels
+    
+    %% remove dimensions of zero variance (or learning algs will crash)
+    variances = var(data, 0, 1);
+    if any(variances == 0)
+        assert(~all(variances == 0));
+        data(:,variances == 0) = [];
+    end
+    
+    %% print info
+    if verbose
+        disp([num2str(numel(uniqueLabel)),'-class ',num2str(size(data, 2)),'-dim ',classifierType]);
+        disp(uniqueLabel(:)');
+        if any(variances == 0)
+            disp(['removed ',num2str(sum(variances==0)),' dims with zero variance']);
+        end
+        t = tic();
+    end
+
+    %% classify
+    if strcmp(classifierType, 'knn') % --- knn ---
+        predLabel = ClassifyKNNHold1Out(classifierParams.k, data', labelNum, classifierParams.distance);
+        acc = sum(predLabel == labelNum) / numel(labelNum);
+        % for knn, score is the "strength" of the classification
+    else
+        error('unexpected classifierType');
+    end
+
+    %% finalize predLabel
+    predLabel = uniqueLabel(predLabel); % convert back from idx into uniqueLabel to labels as they were provided in the input
+
+    if verbose; disp([classifierType,' took ',num2str(toc(t)),' s']); end
+end
---
+++ statistics/SensitivitySpecificity.m
@@ -0,0 +1,39 @@
+% Eli Bowen
+% 10/14/2021
+% https://en.wikipedia.org/wiki/Sensitivity_and_specificity
+% INPUTS:
+%   pred - 1 x n_datapts (logical)
+%   target - 1 x n_datapts (logical)
+% RETURNS:
+%   sensitivity - scalar (numeric) - sensitivity aka true positive rate aka recall aka hitrate
+%       false negative rate = 1 - sensitivity
+%   specificity - scalar (numeric) - specificity aka true negative rate aka selectivity
+%       false positive rate = 1 - specificity
+%   precision - scalar (numeric) - precision aka positive predictive value
+%       false discovery rate = 1 - precision
+%   acc - scalar (numeric) - accuracy aka fraction correct
+%   accBalanced - scalar (numeric) - accuracy, adjusted for unequal N
+%   n_tp - scalar (int-valued numeric) - number of true positives
+%   n_tn - scalar (int-valued numeric) - number of true negatives
+%   n_fp - scalar (int-valued numeric) - number of false positives
+%   n_fn - scalar (int-valued numeric) - number of false negatives
+function [sensitivity,specificity,precision,acc,accBalanced,n_tp,n_tn,n_fp,n_fn] = SensitivitySpecificity (pred, target)
+    validateattributes(pred, 'logical', {'nonempty','vector','numel',numel(target)});
+    validateattributes(target, 'logical', {'nonempty','vector','numel',numel(pred)});
+
+    pred = pred(:)'; % make sure they're the same dimensionality
+    target = target(:)';
+
+    n_tp = sum(pred & target);
+    n_tn = sum(~pred & ~target);
+    n_fp = sum(pred & ~target);
+    n_fn = sum(~pred & target);
+%     n_p = sum(target);
+%     n_n = sum(~target);
+
+    sensitivity = n_tp / (n_tp + n_fn); % aka n_tp / n_p
+    specificity = n_tn / (n_tn + n_fp); % aka n_tn / n_n
+    precision   = n_tp / (n_tp + n_fp);
+    acc = sum(pred == target) / numel(target); % aka (n_tp + n_tn) / (n_p + n_n)
+    accBalanced = (sensitivity + specificity) / 2; % aka ((n_tp / n_p) + (n_tn / n_n)) / 2;
+end
