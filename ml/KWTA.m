88431c70e44c5f8af8b51e1421269581568cb8cc
Eli <mail@elibowen.net>
2022-04-07 00:00:10 -0400

    KWTA can now run on parallel on multiple datapoints

--- ml/KWTA.m
+++ ml/KWTA.m
@@ -1,14 +1,27 @@
 % Eli Bowen 2/2022
 % INPUTS:
-%   data - 1 x n (numeric)
+%   data - d x n (numeric)
 %   k    - scalar (int-valued numeric)
 % RETURNS:
-%   data - 1 x n (numeric) if input is on gpu or sparse, so is output
+%   data - d x n (numeric) if input is on gpu or sparse, so is output
+%   idx  - k x n (numeric index 1 --> d), elements are sorted largest to smallest
 function data = KWTA(data, k)
     validateattributes(data, {'numeric'}, {}, 1);
     validateattributes(k, {'numeric'}, {'nonempty','scalar','integer'}, 2);
+    do_flip = false;
+    if isvector(data) && size(data, 2) > size(data, 1)
+        data = data'; % should be d x 1
+        do_flip = true;
+    end
+    [~,n] = size(data);
 
-    [~,idx] = maxk(data, k);
+    [~,idx] = maxk(data, k, 1); % idx is k x n
     data(:) = 0;
-    data(idx) = 1;
+    for i = 1 : n
+        data(idx(:,i),i) = 1;
+    end
+
+    if do_flip
+        data = data'; % flip back
+    end
 end
