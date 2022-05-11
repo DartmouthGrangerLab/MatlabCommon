7331f58aaeca06b1541a2db9d5629aab60b56f77
Eli <mail@elibowen.net>
2022-05-10 22:33:13 -0400

    Bug fix for calling Plot with color set to redblue

--- figurecode/Plot.m
+++ figurecode/Plot.m
@@ -1,14 +1,16 @@
 % Eli Bowen 12/17/2021
 % less error-prone version of matlab's plot()
 % matlab's plot is annoying about x and y looking just right - this function pleases it
-% also supports the additional name-value argument "alpha", for color transparency
-% also supports color = 'redblue' (gradient red to blue)
-% INPUTS:
+% also supports:
+%   an additional name-value argument "alpha", for color transparency
+%   'Color' = 'redblue' (gradient red to blue)
+% INPUTS
 %   x
 %   y
 %   other arguments to matlab's plot()
-% RETURNS:
+% RETURNS
 %   h - plot handle
+% see also plot
 function h = Plot(x, y, varargin)
     validateattributes(x, {'numeric','logical'}, {'nonempty'}, 1);
     validateattributes(y, {'numeric','logical'}, {'nonempty'}, 2);
@@ -47,8 +49,25 @@ function h = Plot(x, y, varargin)
     idx = find(strcmpi(varargin, 'color'));
     if ~isempty(idx) && strcmpi(varargin{idx+1}, 'redblue')
         varargin([idx,idx+1]) = [];
-        h = plot(x, y, varargin{:});
-        set(h.Edge, 'ColorBinding', 'interpolated', 'ColorData', redblue());
+        
+        if isvector(x)
+            n_pts_in_line = numel(x);
+            h = plot(x, y, varargin{:});
+            drawnow % for some reason this must be right here
+            cmap = redblue(n_pts_in_line);
+            cmap = uint8(255 .* cat(2, cmap, ones(n_pts_in_line, 1))');
+            set(h.Edge, 'ColorBinding', 'interpolated', 'ColorData', cmap); % undocumented matlab
+        else
+            hold on
+            for i = 1 : size(x, 2)
+                n_pts_in_line = size(x, 1);
+                h = plot(x(:,i), y(:,i), varargin{:});
+                drawnow % for some reason this must be right here
+                cmap = redblue(n_pts_in_line);
+                cmap = uint8(255 .* cat(2, cmap, ones(n_pts_in_line, 1))');
+                set(h.Edge, 'ColorBinding', 'interpolated', 'ColorData', cmap); % undocumented matlab
+            end
+        end
     else
         h = plot(x, y, varargin{:});
     end
